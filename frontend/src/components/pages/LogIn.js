79a956b05dffb179756f765eec7c2960a047695f
eusebiu <raduflorin18@stud.ase.ro>
2020-12-04 17:18:17 +0200

    LogIn CSS V1

--- frontend/src/components/pages/LogIn.js
+++ frontend/src/components/pages/LogIn.js
@@ -17,16 +17,18 @@ function LogIn() {
       <div>
         <Header name={"Log In"} />
 
-        <h1>Bine ati venit la maimute</h1>
-        <h2>Log in or sign up</h2>
-
+        <h1 id="Title">Bine ati venit la maimute</h1>
+        <h2 id="Login">Log in or sign up</h2>
+        <div id="divLogIn">
         <label for="username">Username: </label>
         <input type="text" id="username" onChange={handleChangeUsername}/>
         <label for="password">Password: </label>
         <input type="text" id="password" onChange={handleChangePassword}/>
-
-        <input type="button" value="Log in"/>
-        <a href="www.google.com">Sign up</a>
+        
+        <input type="button" value="Log in" id="btnLogIn"/>
+        <a href="www.google.com" id="SignUp">Sign up</a>
+        </div>
+        
       </div>
     );
   }
