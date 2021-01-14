84a810398e7876a1d3df549cdbc378fc4556a8b4
eusebiu <raduflorin18@stud.ase.ro>
2021-01-14 16:47:35 +0200

    Editare-notita

--- frontend/src/components/pages/EditareNotita.js
+++ frontend/src/components/pages/EditareNotita.js
@@ -1,14 +1,42 @@
-import React, { useState, useContext, useEffect } from 'react';
-import Header from '../headers/Header';
+import React, { useState } from 'react';
+import { EditorState } from 'draft-js';
+import { Editor } from 'react-draft-wysiwyg';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import '../pagesStyle/EditareNotita.css';
+import { convertToHTML } from 'draft-convert';
 
-function EditareNotita(){
-    
-    return (
-        <div >
-          <Header name={"Editare notita"} />
-        
-        </div>
-    );
+const EditareNotita = () => {
+
+
+  const [editorState, setEditorState] = useState(
+    () => EditorState.createEmpty(),
+  );
+
+  const  [convertedContent, setConvertedContent] = useState(null);
+  const handleEditorChange = (state) => {
+    setEditorState(state);
+    convertContentToHTML();
+  }
+  const convertContentToHTML = () => {
+    let currentContentAsHTML = convertToHTML(editorState.getCurrentContent());
+    setConvertedContent(currentContentAsHTML);
+  }
+
+  return (
+    <div className="EditareNotita">
+      <header className="EditareNotita-header">
+        Rich Text Editor Example
+        </header>
+      <Editor
+        defaultEditorState={editorState}
+        onEditorStateChange={handleEditorChange}
+        wrapperClassName="wrapper-class"
+        editorClassName="editor-class"
+        toolbarClassName="toolbar-class"
+      />
+    </div>
+  )
+
 }
 
 export default EditareNotita;
